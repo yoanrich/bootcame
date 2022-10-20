ba3959546cde397c19318bb4bee77314d425c468
cselina2000 <cselina2000@gmail.com>
2022-10-20 18:54:52 +0700

    sorting

--- weak2/tugas/shorting.js
+++ weak2/tugas/shorting.js
@@ -1,4 +1,10 @@
-const points = [40, 100, 1, 5, 25, 10];
-points.sort(function (a, b) { return a - b });
 
-console.log(points);
+let angka = [12, 45, 76, 23, 1, 45, 75, 23, 65, 4];
+
+let big = angka.sort(function (kecil, besar) { return besar - kecil }); console.log(big);
+
+console.log('break');
+
+let number = [12, 45, 76, 23, 1, 45, 75, 23, 65, 4];
+
+let small = number.sort(function (kecil, besar) { return kecil - besar }); console.log(small);
