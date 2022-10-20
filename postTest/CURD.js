04f7be348672a5839052507eae8c75cdc8b6a3aa
cselina2000 <cselina2000@gmail.com>
2022-10-20 18:14:28 +0700

    CURD berhasil

--- postTest/CURD.js
+++ postTest/CURD.js
@@ -1,59 +1,58 @@
-let nik = [
-    "162082",
-    "162083",
-    "162085",
-    "162086"
-];
-
-let nama = [
-    "ayu",
-    "andini",
-    "reza",
-    "arabian"
-];
-
-let umur = [
-    "20",
-    "20",
-    "24",
-    "24"
-];
+let data = [];
 
 function tampil() {
+    // console.log('fungsitampil');
     let tabel = document.getElementById("tabel");
     tabel.innerHTML = "<tr><th>No</th><th>NIK</th><th>NAMA</th><th>UMUR</th><th>ACTION</th></tr>";
-    for (let i = 0; i < nik.length; i++) {
+    for (let i = 0; i < data.length; i++) {
         let btnEdit = "<button class ='btn-edit' href = '#' onclick = 'edit (" + i + ")'>Edit</button>";
         let btnHapus = "<button class = 'btn-hapus' href = '#' onclick = 'hapus (" + i + ")'>Hapus</button>";
         j = i + 1;
-        tabel.innerHTML += "<tr><td>" + j + "</td><td>" + nik[i] + "</td><td>" + nama[i] + "</td><td>" + umur[i] + "</td><td>" + btnEdit + "" + btnHapus + "</td ></tr > ";
+        // console.log(data[i]);
+        tabel.innerHTML += "<tr><td>" + j + "</td><td>" + data[i].nik + "</td><td>" + data[i].nama + "</td><td>" + data[i].umur + "</td><td>" + btnEdit + "" + btnHapus + "</td ></tr > ";
     }
 }
 
 function tambah() {
-    var input = document.querySelector("input[name=nik]");
-    var input = document.querySelector("input[name=nama]");
-    var input = document.querySelector("input[name=umur]");
-    nik.push(input.value);
-    nama.push(input.value);
-    umur.push(input.value);
-    input.value = "";
-    tampil();
+    let inputnik = document.querySelector("input[name=nik]");
+    let inputnama = document.querySelector("input[name=nama]");
+    let inputumur = document.querySelector("input[name=umur]");
+    // agar inputan tidak kosong tapi blm jadi
+    if (data.inputnik === "") {
+        alert("tidak boleh kosong")
+    }
+    else if (data.inputnama === "") {
+        alert("tidak boleh kosong")
+    } else if (data.inputni === "") {
+        alert("tidak boleh kosong")
+    } else {
+        data.push({
+            nik: parseInt(inputnik.value),
+            nama: inputnama.value,
+            umur: parseInt(inputumur.value)
+        });
+    }
 
+    // mereset inputan
+    inputnik.value = "";
+    inputnama.value = "";
+    inputumur.value = "";
+    tampil();
 }
 
 function edit(id) {
-    let baru = prompt("Edit NIK", nik[id]);
-    nik[id] = baru;
-    baru = prompt("Edit NAMA", nama[id]);
-    nama[id] = baru;
-    baru = prompt("Edit Umur", umur[id]);
-    umur[id] = baru;
+    let baruNik = prompt("Edit NIK", data[id].nik);
+    data[id].nik = baruNik;
+    let baruNama = prompt("Edit NAMA", data[id].umur);
+    data[id].nama = baruNama;
+    let baruUmur = prompt("Edit Umur", data[id].umur);
+    data[id].umur = baruUmur;
     tampil();
 }
 
 function hapus(id) {
-    nik.pop(id);
+    data.splice(id, 1);
     tampil();
 }
+
 tampil();
